04b9b06b4aabc9e9e9a16c17dd293d9050351420
Zakaria Makhlouf <ton.email@example.com>
2025-05-14 11:01:52 +0200

    a faire

--- tp2/src/main/java/fr/univ_orleans/iut45/r207/tp2/Propities.java
+++ tp2/src/main/java/fr/univ_orleans/iut45/r207/tp2/Propities.java
@@ -8,23 +8,63 @@ import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.builder.GraphTypeBuilder;
 import org.jgrapht.util.SupplierUtil;
 
-
 public class Propities {
-    public static Set<String> unionVoisinages(Graph<String, DefaultEdge> graph, String u, String v) {
+    public Set<String> unionVoisinages(Graph<String, DefaultEdge> graph, String u, String v) {
         Set<String> union = new HashSet<>();
-        
 
         if (!graph.containsVertex(u) || !graph.containsVertex(v)) {
             return union;
         }
 
-        for(DefaultEdge e : graph.edgesOf(v)){
-            union.add(graph.getEdgeTarget(e));    
+        for (DefaultEdge e : graph.edgesOf(v)) {
+            if (graph.getEdgeSource(e).equals(u)) {
+                union.add(graph.getEdgeTarget(e));
+            } else {
+                union.add(graph.getEdgeSource(e));
+            }
         }
-        for(DefaultEdge e : graph.edgesOf(u)){
-            union.add(graph.getEdgeTarget(e));    
+        for (DefaultEdge e : graph.edgesOf(u)) {
+            if (graph.getEdgeSource(e).equals(v)) {
+                union.add(graph.getEdgeTarget(e));
+            } else {
+                union.add(graph.getEdgeSource(e));
+            }
         }
         return union;
-}
-// a faire pour la semaine pro ;
+    }
+    // a faire pour la semaine pro ;
+
+    public static Set<String> lesVoisins(Graph<String, DefaultEdge> graph, String u, String v) {
+        Set<String> voisinsU = new HashSet<>();
+        Set<String> voisinsV = new HashSet<>();
+
+        for (DefaultEdge e : graph.edgesOf(u)) {
+            String source = graph.getEdgeSource(e);
+            String target = graph.getEdgeTarget(e);
+
+            if (source.equals(u) && !target.equals(u)) {
+                voisinsU.add(target);
+            } else if (target.equals(u) && !source.equals(u)) {
+                voisinsU.add(source);
+            }
+        }
+
+        for (DefaultEdge e : graph.edgesOf(v)) {
+            String source = graph.getEdgeSource(e);
+            String target = graph.getEdgeTarget(e);
+
+            if (source.equals(v) && !target.equals(v)) {
+                voisinsV.add(target);
+            } else if (target.equals(v) && !source.equals(v)) {
+                voisinsV.add(source);
+            }
+        }
+        for (String voisin : voisinsV) {
+            if (voisinsU.contains(voisin)) {
+                voisinsU.remove(voisin);
+            }
+        }
+
+        return voisinsU;
+    }
 }
--- tp2/src/test/java/fr/univ_orleans/iut45/r207/tp2/AppTest.java
+++ tp2/src/test/java/fr/univ_orleans/iut45/r207/tp2/AppTest.java
@@ -2,8 +2,12 @@ package fr.univ_orleans.iut45.r207.tp2;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultUndirectedGraph;
 import org.jgrapht.graph.builder.GraphTypeBuilder;
 import org.jgrapht.util.SupplierUtil;
 import org.junit.Test;
@@ -42,4 +46,56 @@ public class AppTest
         assertEquals(1, graph.outDegreeOf(v1));
         assertEquals(0, graph.outDegreeOf(v2));
     }
+
+    @Test
+public void testUnion() {
+    Graph<String, DefaultEdge> graph = new DefaultUndirectedGraph<>(DefaultEdge.class);
+
+    // Ajout des sommets
+    graph.addVertex("a");
+    graph.addVertex("b");
+    graph.addVertex("c");
+    graph.addVertex("d");
+
+    // Ajout des arêtes
+    graph.addEdge("a", "b");
+    graph.addEdge("a", "c");
+    graph.addEdge("b", "c");
+    graph.addEdge("c", "d");
+
+    Propities p = new Propities();
+
+    // Test 1 : union des voisins de a et b
+    Set<String> attendu1 = Set.of("a", "b", "c");  // voisins de a et b
+    assertEquals(attendu1, p.unionVoisinages(graph,"a", "b"));
+
+    // Test 2 : sommet inexistant
+    assertEquals(Set.of(), p.unionVoisinages(graph, "a", "f"));
+}
+
+
+    @Test    
+    public void  lesVoisinsTest(){
+              Graph<String, DefaultEdge> graph = GraphTypeBuilder
+				.undirected()
+				.allowingMultipleEdges(true)
+				.allowingSelfLoops(true)
+				.vertexSupplier(SupplierUtil.createStringSupplier())
+				.edgeSupplier(SupplierUtil.createDefaultEdgeSupplier())
+				.buildGraph();
+
+        String a = graph.addVertex();
+        String b = graph.addVertex();
+        String c = graph.addVertex();
+        String d = graph.addVertex();
+
+        graph.addEdge(a, b);
+        graph.addEdge(a, c);
+        graph.addEdge(c, b);
+        graph.addEdge(b, d);
+        new HashSet<>()
+ assertEquals(, 0);
+    }
+
+
 }
